6f36d72c5ad2ed11b274624f694457423832fa19
?ᴋᴀɴᴀᴏᴋɪ <115692165+kanaok1@users.noreply.github.com>
2025-05-27 16:46:40 +0300

    feature/HBS-13 (#9)
    * HBS-13 Update HotelPhoto model and DB schema to use s3Key instead of URL
    
    * HBS-13 Update hotel_photo table schema
    
    * HBS-13 Rename hotel_photo column url to s3key and enforce unique constraint
    
    * HBS-13 Update hotel_photo table: alter s3key column type and add unique constraint
    
    ---------
    
    Co-authored-by: Степаненко Иван Дмитриевич <dima@it-top.isues>

---
+++ src/main/resources/db/changelog/changeset/007_update-hotel-photo-table.sql
@@ -0,0 +1,13 @@
+-- liquibase formatted sql
+
+-- changeset kanaoki:1748348440547-1
+ALTER TABLE hotel_photo RENAME COLUMN url TO s3key;
+
+-- changeset kanaoki:1748348440547-2
+ALTER TABLE hotel_photo ALTER COLUMN s3key TYPE VARCHAR(255);
+
+-- changeset kanaoki:1748348440547-3
+ALTER TABLE hotel_photo ALTER COLUMN s3key SET NOT NULL;
+
+-- changeset kanaoki:1748348440547-4
+ALTER TABLE hotel_photo ADD CONSTRAINT uc_hotelphoto_s3key UNIQUE (s3key);
