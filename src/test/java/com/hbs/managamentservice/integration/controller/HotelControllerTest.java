7a4fc52d30d7494e4d0ac6dc84dc8e3f940bd374
Ворончихин Иван Романович <bonoberkut@gmail.com>
2025-05-31 20:10:17 +0300

    HBS-8: Add pagination support and detailed hotel fetching in HotelControllerImpl and related classes.

--- src/test/java/com/hbs/managamentservice/integration/controller/HotelControllerTest.java
+++ src/test/java/com/hbs/managamentservice/integration/controller/HotelControllerTest.java
@@ -1,14 +1,20 @@
 package com.hbs.managamentservice.integration.controller;
 
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
 import com.hbs.managamentservice.dto.request.CreateHotelRequest;
 import com.hbs.managamentservice.dto.request.LocationRequest;
 import com.hbs.managamentservice.dto.response.HotelResponse;
 import com.hbs.managamentservice.dto.response.LocationResponse;
+import com.hbs.managamentservice.dto.response.PagedResponse;
 import com.hbs.managamentservice.model.HotelStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,12 +25,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@DBRider
+@DBUnit(caseSensitiveTableNames = true, schema = "public")
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HotelControllerTest {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+
+    @Test
+    @DataSet(value = {"dataset/location/locations.yaml", "dataset/hotel/hotels.yaml"}, cleanBefore = true)
+    void testGetAllHotels() {
+        ResponseEntity<PagedResponse<HotelResponse>> response = testRestTemplate.exchange(
+                "/api/v1/hotels?page=0&size=10",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        PagedResponse<HotelResponse> hotels = response.getBody();
+        assertNotNull(hotels);
+        assertThat(hotels.content()).isNotEmpty();
+    }
+
+    @Test
+    @DataSet(value = {"dataset/location/locations.yaml", "dataset/hotel/hotels.yaml"}, cleanBefore = true)
+    void testGetHotelById() {
+        ResponseEntity<HotelResponse> response = testRestTemplate.getForEntity(
+                "/api/v1/hotels/1",
+                HotelResponse.class
+        );
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        HotelResponse hotelResponse = response.getBody();
+        assertNotNull(hotelResponse);
+        assertThat(hotelResponse.id()).isEqualTo(1);
+    }
 
     @Test
     void testCreateHotel() {
